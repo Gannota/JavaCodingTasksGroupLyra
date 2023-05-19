433412e33040bce8b8a3712793fa3d490e7d1a28
roman <romanaliev808@gmail.com>
2023-05-18 22:31:57 -0500

    divide 2 numbers without /

---
+++ src/Roman_week1_superSolution/Divide_number.java
@@ -0,0 +1,33 @@
+package Roman_week1_superSolution;
+
+import java.util.Scanner;
+
+public class Divide_number {
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter first number");
+
+        Scanner scan = new Scanner(System.in);
+
+        int num1 = scan.nextInt();
+
+        System.out.println("Enter second number");
+        int num2 = scan.nextInt();
+
+        int result = 0;
+
+        while(num1 >= num2){
+            num1 = num1 - num2;
+            result ++;
+
+        }
+
+
+
+        System.out.println(" Output of dividing 2 numbers is  " + result);
+
+
+
+    }
+}
