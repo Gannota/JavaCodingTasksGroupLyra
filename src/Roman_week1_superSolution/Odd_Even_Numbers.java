1b1ca25c89c81560870b74a5788796aa904e3962
roman <romanaliev808@gmail.com>
2023-05-18 21:39:01 -0500

    odd and even number solution

---
+++ src/Roman_week1_superSolution/Odd_Even_Numbers.java
@@ -0,0 +1,29 @@
+package Roman_week1_superSolution;
+
+import java.util.Scanner;
+
+public class Odd_Even_Numbers {
+
+    public static void main(String[] args) {
+
+       /* int num = 4;
+
+       if( num %2 == 0){
+           System.out.println(num + " is even number");
+       }else {
+           System.out.println(num + " is odd number");
+       }
+       */
+
+        Scanner scan = new Scanner(System.in);
+       int num1 = scan.nextInt();
+
+       if(num1 %2 !=0){
+           System.out.println(num1 + " is odd number");
+       }else {
+           System.out.println(num1 + " is even number");
+       }
+       scan.close();
+
+    }
+}
