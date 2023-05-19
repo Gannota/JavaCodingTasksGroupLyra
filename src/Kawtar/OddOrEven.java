d846d19593ed8a2123749c2db21ae8a2dbcd3898
KawtarCydeo <K.tiq93@gmail.com>
2023-05-19 15:58:47 -0500

    Lyra Week1 Java

---
+++ src/Kawtar/OddOrEven.java
@@ -0,0 +1,16 @@
+package Kawtar;
+
+public class OddOrEven {
+    public static void main(String[] args) {
+        int number=8;
+
+        if(number%2==0) {
+            System.out.println(number + "number is even");
+        }else{
+            System.out.println(number +"Number is Odd ");
+        }
+    }
+
+
+}
+
