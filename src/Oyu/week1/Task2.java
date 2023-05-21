e34c8f11afc0f132a371cd9c4b5ed190324113c2
OyuBuren <obmglub@gmail.com>
2023-05-20 23:08:17 -0500

    Task2.java was created

--- src/Oyu/week1/Task2.java
+++ src/Oyu/week1/Task2.java
@@ -1,4 +1,21 @@
 package Oyu.week1;
 
 public class Task2 {
+
+    public static int divideTwoNumbers(int dividend, int divisor){
+
+        if(divisor ==0){
+            throw new ArithmeticException("Can not divide by zero");
+        } else if (dividend==0) {
+            return 0;
+        }
+        return dividend/divisor;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(divideTwoNumbers(50, 2));
+
+
+    }
 }
