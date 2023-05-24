53f1e80474217e582c9794ccc0ae39342475478e
OyuBuren <obmglub@gmail.com>
2023-05-24 17:21:52 -0500

    Runner updated

--- src/Oyu/week2/Runner.java
+++ src/Oyu/week2/Runner.java
@@ -9,5 +9,22 @@ public class Runner {
         System.out.println(Task4.Reverse("Java"));
         System.out.println(Task4.reverse("Wooden Spoon"));
 
+        System.out.println("-----------------------------------------------");
+
+        System.out.println("Frequency of character test started");
+
+        System.out.println(Task5.FrequencyOfChars("HHadddjeee"));
+        System.out.println(Task5.FrequencyOfChar("aaaaaaaaaafffffffffffffffggggggggggggggggggg"));
+        System.out.println(Task5.FrequencyOfCharacter("AAABBCDD"));
+        System.out.println(Task5.frequency("AAABBCDD"));
+
+
+        System.out.println("------------------------------------------------");
+
+        System.out.println("Valid password test started");
+
+        System.out.println(Task6.PassWordvalidation("@CydeoSchool2023"));
+        System.out.println(Task6.PassWordvalidation("WoodenSpppon"));
+
     }
 }
