fc2a2577b34a954f9286ec043a7f7362016eb599
OyuBuren <obmglub@gmail.com>
2023-05-24 16:48:51 -0500

    Runner class was created

---
+++ src/Oyu/week2/Runner.java
@@ -0,0 +1,13 @@
+package Oyu.week2;
+
+public class Runner {
+
+    public static void main(String[] args) {
+        System.out.println("Reverse String test started");
+
+        System.out.println(Task4.strReverse("ABDC"));
+        System.out.println(Task4.Reverse("Java"));
+        System.out.println(Task4.reverse("Wooden Spoon"));
+
+    }
+}
