5d70828469903ab2c88c8ec94abd0c53a9bade12
OyuBuren <obmglub@gmail.com>
2023-05-24 16:48:30 -0500

    Task4 completed

--- src/Oyu/week2/Task4.java
+++ src/Oyu/week2/Task4.java
@@ -1,12 +1,40 @@
 package Oyu.week2;
 
 public class Task4 {
-    public static void main(String[] args) {
 
+    /*
+    Write a return method that can reverse String
+     */
 
-
-
-
-
+    public static String reverse(String str) {
+        String reverse = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse +=str.charAt(i) ;
+        }
+        return reverse;
     }
+    public static String strReverse(String str) {
+        String reverse = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse += str.toCharArray()[i];
+        }
+        return reverse;
+    }
+
+    public static String Reverse(String str) {
+        return new StringBuffer(str).reverse().toString();
+    }
+
+
+
+
+
+
+
+
+
 }
+
+
+
+
