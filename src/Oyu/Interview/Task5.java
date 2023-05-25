1f3b27e641b182af3cb8343955c8d7dd30bec7fe
OyuBuren <obmglub@gmail.com>
2023-05-25 18:22:37 -0500

    Easiest and samplist Frequency of Char method added Task5.FeOfChar

--- src/Oyu/Interview/Task5.java
+++ src/Oyu/Interview/Task5.java
@@ -9,13 +9,29 @@ public class Task5 {
     public static void main(String[] args) {
 
         System.out.println(frequency("AABBBDDDDCCCCCCC"));
+
+        String str = "aaabbbbccccc";
+        System.out.println(FeOfChar(str));
+
+
     }
     /*
     Write a return method that can find the frequency of
     characters
      */
 
+    public static String FeOfChar(String str) {
+        String result = "";
+        for (String each : str.split("")) {
+            int frequency = Collections.frequency(Arrays.asList(str.split("")), each);
+            if (!result.contains(each)) {
+                result += each + frequency;
+            }
+        }
 
+
+        return result;
+    }
 
     //Solution1
     public static String FrequencyOfChars(String str) {
@@ -38,37 +54,36 @@ public class Task5 {
     //solution2
     public static String FrequencyOfChar(String str) {
         String expectedResult = "";
-        int j=0;
-        while( j < str.length()) {
+        int j = 0;
+        while (j < str.length()) {
             int count = 0;
-            for(int i=0; i < str.length(); i++) {
-                if(str.charAt(i) == str.charAt(j)) {
+            for (int i = 0; i < str.length(); i++) {
+                if (str.charAt(i) == str.charAt(j)) {
                     count++;
                 }
             }
-            expectedResult +=str.charAt(j)+"" + count;
-            str = str.replace(""+str.charAt(j) , "" );
+            expectedResult += str.charAt(j) + "" + count;
+            str = str.replace("" + str.charAt(j), "");
         }
         return expectedResult;
     }
 
     // solution3
     public static String FrequencyOfCharacter(String str) {
-        String b=new LinkedHashSet<>(Arrays.asList(str.split(""))).toString();
-        b = b.replace(", ","").replace("[","").replace("]","");
-        String result="";
+        String b = new LinkedHashSet<>(Arrays.asList(str.split(""))).toString();
+        b = b.replace(", ", "").replace("[", "").replace("]", "");
+        String result = "";
 
-        for(int j=0; j<b.length();j++) {
-            int count=0;
+        for (int j = 0; j < b.length(); j++) {
+            int count = 0;
 
-            for(int i=0; i<str.length(); i++)
-                if(str.substring(i, i+1).equals(""+str.charAt(j)))
+            for (int i = 0; i < str.length(); i++)
+                if (str.substring(i, i + 1).equals("" + str.charAt(j)))
                     count++;
-            result+=b.substring(j, j+1)+count;
+            result += b.substring(j, j + 1) + count;
         }
         return result;
     }
-
 
 
     //solution4
