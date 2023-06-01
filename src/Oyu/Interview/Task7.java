fdf292a23373eee339cc4eef179163106a4e25d2
OyuBuren <obmglub@gmail.com>
2023-06-01 06:42:18 -0500

    Task 7 was updated

--- src/Oyu/Interview/Task7.java
+++ src/Oyu/Interview/Task7.java
@@ -1,4 +1,36 @@
 package Oyu.Interview;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Task7 {
+    /*
+     Write a method that can find the maximum number from an int
+            Array
+     */
+    public static void main(String[] args) {
+
+
+
+    }
+    /*
+    public static int maxValueArrayElement(int [] n){
+       Arrays.sort(n);
+        return n[n.length-1];
+   }
+    public static int maxValueArrElement(int [] n){
+
+        int max = Integer.MIN_VALUE;
+        for( int each : n){
+            if(each > max){
+                max=each;
+            }
+        }
+        return max;
+    }
+     */
+
 }
