05116f3df6ea8d299536f7d34882b2208956ae20
OyuBuren <obmglub@gmail.com>
2023-06-02 07:21:54 -0500

    Task 7 was updated and presented to the group

--- src/Oyu/Interview/Task7.java
+++ src/Oyu/Interview/Task7.java
@@ -13,8 +13,24 @@ public class Task7 {
      */
     public static void main(String[] args) {
 
+        int [] arr ={15,42,4,15,150};
+        int [] arr1 ={15,42,4,15,250};
 
-
+        System.out.println("maxArray(arr) = " + maxArray(arr));
+        System.out.println("maxArray1(arr) = " + maxArray1(arr1));
+    }
+    public static int maxArray (int [] n){
+        Arrays.sort(n);
+        return n[n.length-1];
+    }
+    public static int maxArray1 ( int [] num){
+        int max = Integer.MIN_VALUE;
+        for(int each : num){
+            if(each > max){
+                max=each;
+            }
+        }
+        return max;
     }
     /*
     public static int maxValueArrayElement(int [] n){
