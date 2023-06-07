357f27721de6c2d24ed5fd2463e766aee3c5c756
AidaCydeo <stenkovaaida@gmail.com>
2023-06-07 12:29:50 -0500

    devisble by 3,5,15 changed

--- src/Aida/Week2/Devisible_by_3_5_15.java
+++ src/Aida/Week2/Devisible_by_3_5_15.java
@@ -3,20 +3,36 @@ package Aida.Week2;
 public class Devisible_by_3_5_15 {
     public static void main(String[] args) {
 
-        int number = 65;
+     /*  int number = 65;
 
         boolean devisibleBy3 = number%3==0;
         boolean devisibleBy5 = number%5==0;
         boolean devisibleBy15=number%15==0;
 
-        if (number%3==0){
-            System.out.println("the number is devisible by 3");
-        } else if (number%5==0) {
-            System.out.println("the number is devisible by 5");
-        }else if(number%15==0){
-            System.out.println("the number is devisible by 15");
-        }else{
-            System.out.println("the number is not devisible by 3,5,15");
+        System.out.println("--------------------------");
+
+*/
+        System.out.println("Divided by 3: ");
+        for (int i=1; i<100; i++) {
+            if (i%3==0)
+                System.out.print(i +", ");
         }
+
+        System.out.println("\nDivided by 5: ");
+        for (int i=1; i<100; i++) {
+            if (i%5==0) System.out.print(i +", ");
+        }
+
+        System.out.println("\nDivided by 3 & 5: ");
+        for (int i=1; i<100; i++) {
+            if (i%3==0 && i%5==0) System.out.print(i +", ");
+        }
+
+        System.out.println("\nDivided by 15: ");
+        for (int i =1; i<100; i++){
+            if (i%15==0) System.out.println(i +", ");
+            }
+        }
+
+
     }
-}
