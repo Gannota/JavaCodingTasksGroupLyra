db131525e94f402f8f92a927319e3b57dc440930
AidaCydeo <stenkovaaida@gmail.com>
2023-06-06 22:55:32 -0500

    devisble by 3,5,15 created

--- src/Aida/Week2/Devisible_by_3_5_15.java
+++ src/Aida/Week2/Devisible_by_3_5_15.java
@@ -1,4 +1,22 @@
 package Aida.Week2;
 
 public class Devisible_by_3_5_15 {
+    public static void main(String[] args) {
+
+        int number = 65;
+
+        boolean devisibleBy3 = number%3==0;
+        boolean devisibleBy5 = number%5==0;
+        boolean devisibleBy15=number%15==0;
+
+        if (number%3==0){
+            System.out.println("the number is devisible by 3");
+        } else if (number%5==0) {
+            System.out.println("the number is devisible by 5");
+        }else if(number%15==0){
+            System.out.println("the number is devisible by 15");
+        }else{
+            System.out.println("the number is not devisible by 3,5,15");
+        }
+    }
 }
