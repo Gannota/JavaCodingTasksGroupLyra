88891fa5aea47f402e2d6aee5a6439d5b8172b7d
OyuBuren <obmglub@gmail.com>
2023-06-17 08:17:47 -0500

    week 4 task was completed

--- src/Oyu/week5/ReverseString.java
+++ src/Oyu/week5/ReverseString.java
@@ -1,4 +1,18 @@
 package Oyu.week5;
 
 public class ReverseString {
+
+    public static void main(String[] args) {
+        System.out.println("reverse1(\"abcd\") = " + reverse1("abcd"));
+    }
+
+    public static String reverse1(String str){
+        String reverse="";
+
+        for( int i = str.length()-1; i >=0; i--){
+            reverse+=str.charAt(i);
+        }
+        return reverse;
+    }
+
 }
